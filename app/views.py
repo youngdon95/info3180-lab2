449e7e02b9186f3aca77421793c82976f1afdc19
champion_boy <romarioomartin@hotmail.com>
2017-02-05 20:08:47 +0000

    final commit

---
+++ app/views.py
@@ -0,0 +1,75 @@
+"""
+Flask Documentation:     http://flask.pocoo.org/docs/
+Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
+Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+This file creates your application.
+"""
+
+import os
+import time
+from app import app
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
+from werkzeug.utils import secure_filename
+UPLOAD_FOLDER = '/app/static/uploads'
+
+###
+# Routing for your application.
+###
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+@app.route('/')
+def home():
+    """Render website's home page."""
+    return render_template('home.html')
+
+
+@app.route('/about/')
+def about():
+    """Render the website's about page."""
+    return render_template('about.html', name="Mary Jane")
+
+@app.route('/profile')
+def profile():
+    """Render website's home page."""
+    return render_template('profile.html')
+
+@app.route('/timeinfo')
+def timeinfo():
+        
+    return render_template('profile.html', time=time.strftime("%c")) 
+
+
+    ##return render_template('profile.html' time=time)
+## date and time representation
+##print "Current date & time " + time.strftime("%c")
+
+
+###
+# The functions below should be applicable to all Flask apps.
+###
+
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0",port="8080")
